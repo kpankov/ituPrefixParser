e97c56ad65e1003c6122a5292c9577fb8b219040
Konstantin Pankov <kspzel@gmail.com>
2019-05-15 02:45:26 +0300

    Fix getInfo() function

---
+++ ituPrefixParser.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var data = fs.readFileSync('ituPrefixTable.json', 'utf8');
+var words = JSON.parse(data);
+
+var countries = words["countries"];
+
+module.exports.getInfo = function (callsign) {
+    
+    var output;
+    var found = 0;
+    
+    countries.forEach(function (element) {
+        if (callsign.match(element.prefix) ) {
+            output = element;
+            found++;
+        }
+    });
+    
+    if (found > 1) {
+        throw "Multiple countries found!";
+    }
+    
+    if (found === 0) {
+        throw "No country found!";
+    }
+    
+    delete output.prefix;
+
+    return output;
+};
