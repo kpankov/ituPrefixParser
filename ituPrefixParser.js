b10744e12e1ba2102bd5f45f68f84580383fd4d4
Konstantin Pankov <kspzel@gmail.com>
2020-03-13 17:26:28 +0300

    Update database (not complete)

--- ituPrefixParser.js
+++ ituPrefixParser.js
@@ -5,27 +5,43 @@ var words = JSON.parse(data);
 
 var countries = words["countries"];
 
+const findDuplicates = (arr) => {
+  let sorted_arr = arr.slice().sort(); // You can define the comparing function here.
+  // JS by default uses a crappy string compare.
+  // (we use slice to clone the array so the
+  // original array won't be modified)
+  let results = [];
+  for (let i = 0; i < sorted_arr.length - 1; i++) {
+    if (sorted_arr[i + 1] == sorted_arr[i]) {
+      results.push(sorted_arr[i]);
+    }
+  }
+  return results;
+}
+
 module.exports.getInfo = function (callsign) {
-    
-    var output;
+
+    var output = null;
     var found = 0;
-    
+
     countries.forEach(function (element) {
         if (callsign.match(element.prefix) ) {
             output = element;
             found++;
         }
     });
-    
+
     if (found > 1) {
         throw "Multiple countries found!";
+        return null;
     }
-    
+
     if (found === 0) {
         throw "No country found!";
+        return null;
     }
-    
-    delete output.prefix;
 
     return output;
 };
+
+console.log(`The duplicates in ${duplicatedArray} are ${findDuplicates(duplicatedArray)}`);
--- ituPrefixParserTest.js
+++ ituPrefixParserTest.js
@@ -1,5 +1,11 @@
 var ituParser = require("./ituPrefixParser");
 
-console.log(ituParser.getInfo("R2APN"));
+// console.log(ituParser.getInfo("R2APN"));
 
-//console.log(ituParser.getInfo("r2apn"));
+// console.log(ituParser.getInfo("3VAJHS"));
+
+// console.log(ituParser.getInfo("G34GDF"));
+
+console.log(ituParser.getInfo("234GDF"));
+
+console.log(ituParser.getInfo("CQ3KHKJ"));
