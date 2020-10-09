92b5165242b302f6d07979d7c986c057d70d6f5b
Konstantin Pankov <kspzel@gmail.com>
2020-10-10 02:28:35 +0300

    Clean up

--- ituPrefixParserTest.js
+++ ituPrefixParserTest.js
@@ -1,10 +1,10 @@
 var ituParser = require("./ituPrefixParser");
 
-// console.log(ituParser.getInfo("R2APN"));
+console.log(ituParser.getInfo("R2APN"));
 
-// console.log(ituParser.getInfo("3VAJHS"));
+console.log(ituParser.getInfo("3VAJHS"));
 
-// console.log(ituParser.getInfo("G34GDF"));
+console.log(ituParser.getInfo("G34GDF"));
 
 console.log(ituParser.getInfo("234GDF"));
 
