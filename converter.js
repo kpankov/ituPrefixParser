381fd608217a676f549d5dd88ad9693b6dfe83f1
Konstantin Pankov <kspzel@gmail.com>
2020-10-10 03:02:24 +0300

    Flag copy

--- converter.js
+++ converter.js
@@ -3,27 +3,20 @@
 var fs = require('fs');
 var path = require('path');
 
-var data = fs.readFileSync(path.resolve(__dirname) + '/ituPrefixTable.json', 'utf8');
-var words = JSON.parse(data);
-var countries = words["countries"];
+// var ituPrefixTable = fs.readFileSync(path.resolve(__dirname) + '/ituPrefixTable.json', 'utf8');
+var ituPrefixTable = JSON.parse(fs.readFileSync(path.resolve(__dirname) + '/ituPrefixTable.json', 'utf8'));
+var list_itu = ituPrefixTable["countries"];
 
-var country_base = fs.readFileSync(path.resolve(__dirname) + '/countries.json', 'utf8');
-var country_words = JSON.parse(country_base);
-var list = country_words["countries"];
+// var countries = fs.readFileSync(path.resolve(__dirname) + '/countries.json', 'utf8');
+var countries = JSON.parse(fs.readFileSync(path.resolve(__dirname) + '/countries.json', 'utf8'));
+var list_flags = countries["countries"];
 
-// countries.forEach(function (element) {
-//     console.log("name: "+element.name);
-// });
-
-const offset = 127397;
-
-list.forEach(function (element) {
-  if (element.code) {
-    const f = element.code.codePointAt(0);
-    const s = element.code.codePointAt(1);
-    element.flag = String.fromCodePoint(f + offset) + String.fromCodePoint(s + offset);
-    console.log(element.flag + "  " + element.name);
-  }
+list_itu.forEach(function (element) {
+    list_flags.forEach(function (element2) {
+        if (element.name === element2.name) {
+            element.flag = element2.flag;
+        }
+    })
 });
 
-fs.writeFileSync(path.resolve(__dirname) + '/ituPrefixTable_new.json', JSON.stringify(country_words), 'utf8');
+fs.writeFileSync(path.resolve(__dirname) + '/ituPrefixTable_new.json', JSON.stringify(ituPrefixTable), 'utf8');
